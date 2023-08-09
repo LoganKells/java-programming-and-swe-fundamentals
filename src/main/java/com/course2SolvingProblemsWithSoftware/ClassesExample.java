3d0fa49a726133c8bdc526545f6e8b7839e0e29f
LoganKells <lkells@outlook.com>
2023-08-09 19:29:40 -0400

    Created ClassesExample

---
+++ src/main/java/com/course2SolvingProblemsWithSoftware/ClassesExample.java
@@ -0,0 +1,19 @@
+package com.course2SolvingProblemsWithSoftware;
+
+public class ClassesExample {
+    private String color;
+    public ClassesExample(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public static void main(String[] args) {
+        ClassesExample myClassesExample = new ClassesExample("yellow");
+        System.out.println("My table color is: " + myClassesExample.getColor());
+        var x = (2 + 5.0) % 2;
+        System.out.println(x);
+    }
+}
