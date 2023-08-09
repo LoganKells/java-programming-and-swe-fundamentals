4eb3861b34434e0d5726577788466cdd82a402b5
LoganKells <lkells@outlook.com>
2023-08-09 19:18:49 -0400

    Created HelloWorld

---
+++ src/main/java/com/course1/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.course1;
+
+import edu.duke.FileResource;
+
+public class HelloWorld {
+	public void runHello () {
+		FileResource res = new FileResource("hello_unicode.txt");
+		for (String line : res.lines()) {
+			System.out.println(line);
+		}
+	}
+
+	public static void main(String[] args) {
+		var myHelloWorld = new HelloWorld();
+		myHelloWorld.runHello();
+	}
+}
